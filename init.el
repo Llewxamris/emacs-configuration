26927d28572ca96ae9a68322a50ca9cd6a437e16
Llewxamris <maxwell.r.haley@gmail.com>
2018-11-10 22:35:49 -0500

    Add auto-compile
    Automatically compilte byte-code from Elisp on load/save.

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 ;; Setup package management
 (require 'package)
+(setq load-prefer-newer t)
 (package-initialize)
 (setq package-enable-at-startup nil)
 
@@ -26,13 +27,22 @@
     ("9fcac3986e3550baac55dc6175195a4c7537e8aa082043dcbe3f93f548a3a1e0" default)))
  '(package-selected-packages
    (quote
-    (go-errcheck zenburn-theme slime hydandata-light-theme eziam-theme atom-one-light-theme yasnippets elfeed-org org-bullets evil-org flycheck flyspell-correct-popup markdown-mode auto-dictionary auto-dictionary-mode magit dired-x evil-leader elisp-slime-nav linum-relative evil))))
+    (auto-compile go-errcheck slime hydandata-light-theme eziam-theme atom-one-light-theme yasnippets elfeed-org org-bullets evil-org flycheck flyspell-correct-popup markdown-mode auto-dictionary auto-dictionary-mode magit dired-x evil-leader linum-relative evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;; auto-compile: Automatically compile Emacs Lisp libraries
+;;; https://github.com/emacscollective/auto-compile
+(use-package auto-compile
+  :ensure t
+  :config
+  (progn
+    (auto-compile-on-load-mode)
+    (auto-compile-on-save-mode)))
 
 ;; require custom packages
 (require 'my-evil)
