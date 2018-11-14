b71775a7ad82ba60b0d635cd100393be479d49fd
Llewxamris <maxwell.r.haley@gmail.com>
2018-11-13 21:16:15 -0500

    More configuration for SLIME
    - Added <leader>e for defun eval
    - Set REPL evil state to NORMAL so I can escape with <leader><leader>
    - Added hook to autostart SLIME

--- config/languages/my-lisp.el
+++ config/languages/my-lisp.el
@@ -5,6 +5,9 @@
   :config
   (progn
     (setq slime-contribs '(slime-fancy))
-    (setq inferior-lisp-program "/usr/bin/sbcl")))
+    (setq inferior-lisp-program "/usr/bin/sbcl")
+    (evil-leader/set-key-for-mode 'lisp-mode "e" 'slime-eval-defun)
+    (evil-set-initial-state 'slime-repl-mode 'normal)
+    (add-hook 'lisp-mode-hook 'slime)))
 
 (provide 'my-lisp)
