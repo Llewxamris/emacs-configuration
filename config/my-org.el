330becb41bb6e324b8b61e986e916f7252eb5f81
Llewxamris <maxwell.r.haley@gmail.com>
2018-12-14 14:30:56 -0500

    Enhance org-capture templates
    Improved the org-capture templates by adding better prompts. Added
    the RAW type for captured items that need to be refined. For example,
    a todo item that doesn't have clear deadlines.

--- config/my-org.el
+++ config/my-org.el
@@ -10,8 +10,8 @@
     ;; Auto-enter org-mode on files matching *.org
     (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
     ;; Set the org-agenda to read from the follow directory
-    (setq org-agenda-files (list "~/nc/doc/org/agenda"))
-    (setq org-directory "~/nc/doc/org")
+    (setq org-agenda-files (list "~/doc/org/agenda"))
+    (setq org-directory "~/doc/org")
     ;; Add a timestamp whenever a task is set to done
     (setq org-log-done 'time)
     ;; Use `inbox.org` when capturing
@@ -21,9 +21,9 @@
     ;; Set keywords and quick access keys
     (setq org-todo-keywords
 	  ;; Generic task sequence, including appointment keyword
-	  (quote ((sequence "TODO(t)" "WIP(p)" "APPOINTMENT(a)" "|" "DONE(d)")
+	  (quote ((sequence "TODO(t)" "STARTED(s)" "APPOINTMENT(a)" "|" "DONE(d)")
 		  ;; Sequence for blocked tasks
-		  (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)")
+		  (sequence "RAW WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)")
 		  ;; Sequence for financial tasks
 		  (sequence "EXPENSE(e)" "OVERDUE(o@/!)" "|" "PAID(p)"))))
     ;; Enable fast todo selection
@@ -32,14 +32,29 @@
     ;; %? - Starting cursor position
     ;; %U - Insert an inactive timestamp
     (setq org-capture-templates
-	  (quote (("t" "todo" entry (file (concat org-directory "/inbox.org"))
-		  "* TODO %?\n%U\n")
-		 ("c" "calendar" entry (file+headline (concat org-directory "/todo.org") "Calendar")
-		  "*%?\n%U\n")
-		 ("a" "appointment" entry (file+headline (concat org-directory "/todo.org") "Calendar")
-		  "* APPOINTMENT %?\n%U\n")
-		 ("n" "note" entry (file (concat org-directory "/inbox.org"))
-		  "* %?\n%U\n"))))))
+	  (quote (("t" "Task" entry (file "~/doc/org/agenda/inbox.org")
+		   "* TODO %^{task-name}
+:PROPERTIES:
+:EFFORT: %^{effort|0.5|1|2|3|5|8|13}
+:END:
+SCHEDULED: %^{scheduled}T
+DEADLINE: %^{deadline}T
+%?")
+		  ("T" "Raw task" entry (file "~/doc/org/agenda/inbox.org")
+		   "* RAW %^{task-name}
+%U
+%?")
+		  ("e" "Event" entry (file+headline "~/doc/org/agenda/todo.org" "Calendar")
+		   "* %^{event-name}
+%^{event-datetime}T
+%?")
+		  ("a" "Appointment" entry (file+headline "~/doc/org/agenda/todo.org" "Calendar")
+		   "* APPOINTMENT %^{appointment-name}
+SCHEDULED: %^{start-datetime}T
+DEADLINE: %^{end-datetime}T
+%?")
+		  ("n" "Note" entry (file "~/doc/org/agenda/inbox.org")
+		   "* %^{note-title}\n%?\n%U"))))))
 
 ;;; evil-org: Supplemental evil-mode keybindings to emacs org-mode
 ;;; https://github.com/Somelauw/evil-org-mode
