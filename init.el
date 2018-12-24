5bdae6083403bf4172e09f385d239c5e3118d1a5
Llewxamris <maxwell.r.haley@gmail.com>
2018-12-24 00:38:28 -0500

    Add comments to init.el

--- init.el
+++ init.el
@@ -1,27 +1,37 @@
 ;;;; init.el
-;;; Setup package managment
+;;; Defines mostly just the package management portions of Emacs. Ensures
+;;; that ~use-package~ is available by default and kicks off to the other
+;;; local configuration packages to get everything working.
+
 ;; Setup the package package
 (require 'package)
+
+;; Always prefer the newer version of a package.
 (setq load-prefer-newer t)
+
+;; Initialize the packages.
 (package-initialize)
+
+;; Do not activate installed packages on startup.
 (setq package-enable-at-startup nil)
 
-;; Define the list of directories to search for files to load
+;; Define the list of directories to search for local packages/configuration.
 (add-to-list 'load-path (concat user-emacs-directory "config"))
 (add-to-list 'load-path (concat user-emacs-directory "config" "/languages"))
 (add-to-list 'load-path (concat user-emacs-directory "packages"))
 
-;; Define which archives to use for packages
+;; Define which archives to use for fetching packages remotely.
 (setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
 			 ("org" . "http://orgmode.org/elpa/")
 			 ("gnu" . "http://elpa.gnu.org/packages/")))
 
-;; Auto-download use-package if it isn't already installed
+;; Auto-download use-package if it isn't already installed.
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; Enable use-pacakge
+;; Enable use-package. use-package is used for just about all of the package
+;; management in this configuration.
 (require 'use-package)
 
 ;;; Auto-generated Custom stuff
@@ -31,11 +41,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("9fcac3986e3550baac55dc6175195a4c7537e8aa082043dcbe3f93f548a3a1e0" default)))
+	 (quote
+		("9fcac3986e3550baac55dc6175195a4c7537e8aa082043dcbe3f93f548a3a1e0" default)))
  '(package-selected-packages
-   (quote
-    (elisp-slime-nav elips-slime-nav evil-commentary evil-cleverparens auto-compile go-errcheck slime hydandata-light-theme eziam-theme atom-one-light-theme yasnippets elfeed-org org-bullets evil-org flycheck flyspell-correct-popup markdown-mode auto-dictionary auto-dictionary-mode magit dired-x evil-leader linum-relative evil))))
+	 (quote
+		(smooth-scrolling general elisp-slime-nav elips-slime-nav evil-commentary evil-cleverparens auto-compile go-errcheck slime hydandata-light-theme eziam-theme atom-one-light-theme yasnippets elfeed-org org-bullets evil-org flycheck flyspell-correct-popup markdown-mode auto-dictionary auto-dictionary-mode magit dired-x evil-leader linum-relative evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -49,10 +59,13 @@
   :ensure t
   :config
   (progn
+		;; Auto-compile on both loading and saving.
     (auto-compile-on-load-mode)
     (auto-compile-on-save-mode)))
 
-;;; Define configuration files for other packages and their configuration
+;;; Define configuration files for other packages and their configuration. All
+;;; of these packages are local configs. I won't go into detail of what each
+;;; package does here. Instead, check each individual file for it's purpose.
 ;; General packages
 (require 'my-evil)
 (require 'my-dired-x)
