d00e0e866c2c68ee77749738b3fb416670a5509f
Llewxamris <maxwell.r.haley@gmail.com>
2019-09-08 21:34:19 -0400

    Move most of the evil configuration to config.org
    Only thing left behind is the Agenda rebindings. Those will be addressed
    once I start digging into moving over the org-mode configuration.

--- config/my-evil.el
+++ config/my-evil.el
@@ -5,30 +5,6 @@
  (use-package general
   :ensure t
 	:config
-		;; File manipulation bindings
-		(general-define-key
-		 :states 'normal
-		 :prefix "SPC f"
-		 "w" 'save-buffer
-		 "r" 'find-file)
-
-		;; Buffer creation bindings. These bindings focus on opening special
-		;; buffers, not on closing/modifying existing ones
-		(general-define-key
-		 :states 'normal
-		 :prefix "SPC b"
-		 "b" 'ibuffer
-		 "d" 'dired
-		 "D" 'dired-jump)
-
-    ;; Obsolete, need to be refactored out
-		(general-define-key
-		 :states 'normal
-		 :prefix "SPC"
-		 "SPC" 'other-window
-		 "e" 'eval-last-sexp
-		 "z" 'flyspell-correct-previous-word-generic)
-    
     ;; Org-agenda <leader> bindings
     ;; Org-agenda requires redefining some of the global keys to work
     ;; properly. I've only included those global keys that would make
@@ -44,33 +20,5 @@
       "v" 'split-window-right
       "h" 'split-window-below
       "b" 'ibuffer))
-
-;;; evil - The extensible vi layer for Emacs.
-;;; https://github.com/emacs-evil/evil
-(use-package evil
-  :ensure t
-  :config
-  (progn
-    ;; Evil by default
-    (evil-mode 1)))
-
-;;; evil-cleverparens: Evil normal-state minor-mode for editing lisp-like languages.
-;;; https://github.com/luxbock/evil-cleverparens
-(use-package evil-cleverparens
-  :ensure t
-  :config
-  (progn
-    ;; Hook onto Lisp and Elisp modes
-    (add-hook 'lisp-mode-hook 'evil-cleverparens-mode)
-    (add-hook 'emacs-lisp-mode-hook 'evil-cleverparens-mode)))
-
-;;; evil-commentary: Comment stuff out. A port of vim-commentary
-;;; https://github.com/linktohack/evil-commentary
-(use-package evil-commentary
-  :ensure t
-  :config
-  (progn
-    ;; Enable by default
-    (evil-commentary-mode)))
 
 (provide 'my-evil)
