8c1fcad8c655598d36eba9c89d7de13b0511e0eb
Llewxamris <maxwell.r.haley@gmail.com>
2020-02-29 20:27:56 -0500

    Use general keyword instead of calling function
    As part of keeping things declarative, I am leveraging the `:general`
    keyword in `use-package`. This feels better than calling general as
    part of the config body. Feels more declarative.

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
 ;; Load my literate configuration file.
 (org-babel-load-file (concat user-emacs-directory "config.org"))
 (provide 'init)
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
